128beef4cc580d0a545bfe0df21b3e2b51dc5c4c
Sergen Şahin <44943277+sergio4848@users.noreply.github.com>
2024-08-08 09:12:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,32 +1,46 @@
 import PIL.Image
-
-img = PIL.Image.open("foto.jpg")
-
 import PIL.ExifTags
 
-exif={
+# Resmi açma
+img = PIL.Image.open("foto.jpg")
+
+# EXIF verilerini alma
+exif = {
     PIL.ExifTags.TAGS[k]: v
-    for k,v in img._getexif().items()
+    for k, v in img._getexif().items()
     if k in PIL.ExifTags.TAGS
 }
-exif['GPSInfo']
 
-north = exif['GPSInfo'][2]
-east = exif['GPSInfo'][4]
+# GPS verilerini kontrol etme
+if 'GPSInfo' in exif:
+    gps_info = exif['GPSInfo']
+    
+    # Kuzey ve doğu koordinatlarını alma
+    north = gps_info[2]
+    east = gps_info[4]
+    
+    # Koordinatları yazdırma
+    print("North:", north)
+    print("East:", east)
 
-print(north)
-print(east)
+    # Derece, dakika ve saniyeyi dereceye çevirme
+    def convert_to_degrees(value):
+        d = value[0] + (value[1] / 60.0) + (value[2] / 3600.0)
+        return d
 
-lat = ((((north[0] * 60) + north[1]) * 60) + north[2])/60/60
-long = ((((east[0] * 60) + east[1]) * 60) + east[2])/60/60
+    lat = convert_to_degrees(north)
+    long = convert_to_degrees(east)
 
-lat, long =float(lat),float(long)
+    print("Latitude:", lat)
+    print("Longitude:", long)
 
-print(lat)
-print(long)
+    # Harita oluşturma
+    from gmplot import gmplot
 
-from gmplot import gmplot
-gmap=gmplot.GoogleMapPlotter(lat,long,12)
-gmap.marker(lat,long,"cornflowerblue")
-gmap.draw("harita.html")
-
+    # Not: gmplot için bir API anahtarına ihtiyacınız olabilir. 
+    # Eğer öyleyse, gmap = gmplot.GoogleMapPlotter(lat, long, 12, apikey='YOUR_API_KEY')
+    gmap = gmplot.GoogleMapPlotter(lat, long, 12)
+    gmap.marker(lat, long, "cornflowerblue")
+    gmap.draw("harita.html")
+else:
+    print("Resimde GPS bilgisi bulunamadı.")
